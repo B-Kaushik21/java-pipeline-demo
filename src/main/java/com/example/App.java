9fd1d968ddbb77b93f1bfb17ebfb4dc6884da0be
Kaushik <boddula.kaushik@gmail.com>
2025-10-31 05:41:45 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,6 @@ package main.java.com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins!");
+        System.out.println("hey there");
     }
 }
